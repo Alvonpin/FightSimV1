b1ee857722f1474a05922618641bf63ada8a7d08
Alvonpin <43116734+Alvonpin@users.noreply.github.com>
2019-10-10 15:51:08 +0200

    Initial commit

---
+++ Fighter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV1
+{
+    class Fighter
+    {
+        public string name;
+        private int hp;
+        static private Random generator = new Random();
+
+        public Fighter(string input)
+        {
+            name = input;
+            hp = 100;
+        }
+
+        public int Attack()
+        {
+            int damage = generator.Next(0, 50);
+
+            return damage;
+        }
+
+        public void Hurt(int damage)
+        {
+            hp = hp - damage;
+
+            if (hp < 0)
+            {
+                hp = 0;
+            }
+        }
+
+        public bool IsAlive()
+        {
+            if (hp == 0)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        public int GetHp()
+        {
+            return hp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSimV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name your fighter");
+            Fighter Player = new Fighter(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Name your opponent");
+            Fighter Opponent = new Fighter(Console.ReadLine());
+            Console.Clear();
+
+            do
+            {
+                Player.Hurt(Player.Attack());
+                Opponent.Hurt(Opponent.Attack());
+
+                Console.WriteLine(Player.GetHp());
+                Console.WriteLine(Opponent.GetHp());
+
+                Console.ReadLine();
+
+            }
+            while (Player.IsAlive() == true && Opponent.IsAlive() == true);
+
+            if (Player.IsAlive() == true)
+            {
+                Console.WriteLine(Player.name + " won");
+            }
+
+            else
+            {
+                Console.WriteLine(Opponent.name + " won");
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
